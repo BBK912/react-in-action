ac510613b307fca74c50099e6a625a688d28550f
BBK <chnqbj@gmail.com>
2023-02-26 22:41:56 +0800

    basic

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import { Game } from "./game";
 import { Clock } from "./clock";
-
+import { Toggle } from "./eventhandle";
+import { Greeting } from "./conditional-rendering";
+import { LoginControl } from "./conditional-rendering";
+import {Calculator} from './boilingverdict'
+import {Example} from './hook';
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<div><Clock /><Clock /><Clock /></div>);
+root.render(<Example></Example>);
